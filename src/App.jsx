120a96f7df12b71623ec94e3ab50872b38a03e33
PRN-6 <prinsonnazareth2@gmail.com>
2025-08-08 12:02:10 +0530

    Added some stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './components/navbar/NavBar'
 import Hero from './components/hero/Hero'
+import About from './components/about/About'
 
 const App = () => {
   return (
     <>
       <NavBar />
       <Hero/>
+      <About/>
     </>
   )
 }
---
+++ src/components/about/About.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import './About.css'
+
+const about = () => {
+  return (
+    <div className='about'>
+        
+        about
+    </div>
+  )
+}
+
+export default about
